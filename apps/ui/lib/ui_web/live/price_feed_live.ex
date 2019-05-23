9781c1ad4e405a60dd340f5591caec8bfb519f07
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2019-05-23 07:34:24 +0100

    wip - price direction colours

--- apps/ui/lib/ui_web/live/price_feed_live.ex
+++ apps/ui/lib/ui_web/live/price_feed_live.ex
@@ -13,7 +13,6 @@ defmodule UiWeb.PriceFeedLive do
             <div class="input-group input-group-sm" style="width: 180px;">
               <form phx_change="validate" phx-submit="validate">
                 <input type="text" name="search" class="form-control pull-right" placeholder="Search">
-
                 <div class="input-group-btn">
                   <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
                 </div>
@@ -32,7 +31,12 @@ defmodule UiWeb.PriceFeedLive do
               <%= for tick <- Keyword.values(@ticks) do %>
               <tr>
                 <td><%= tick.symbol %></td>
-                <td><%= tick.price %></td>
+                <td>
+                  <span class="text-<%= if tick.direction == "up" do %>green<% else %>red<% end %>">
+                    <i class="fa fa-angle-<%= tick.direction %>"></i>
+                    <%= tick.price %>
+                  </span>
+                </td>
               </tr>
               <% end %>
             </tbody></table>
@@ -55,9 +59,12 @@ defmodule UiWeb.PriceFeedLive do
     ticks = Hefty.fetch_streaming_symbols()
       |> symbols_to_keywords
 
-      ticks
-        |> Keyword.keys()
-        |> Enum.map(&(UiWeb.Endpoint.subscribe("stream-#{&1}")))
+    ticks
+      |> Keyword.keys()
+      |> Enum.map(&(UiWeb.Endpoint.subscribe("stream-#{&1}")))
+
+    ticks = ticks
+      |> Enum.map(fn {key, data} -> {key, Map.put_new(data, :direction, get_direction())} end)
 
     {:ok, assign(socket, ticks: ticks)}
   end
@@ -72,10 +79,23 @@ defmodule UiWeb.PriceFeedLive do
   end
 
   def handle_info(%{event: "trade_event", payload: event}, socket) do
+
+    socket.assigns.ticks
+    |> Enum.map(&IO.inspect/1)
+
+    old_tick = Keyword.get(
+      socket.assigns.ticks,
+      :"#{event.symbol}"
+    )
+
+    IO.inspect(Float.parse(old_tick.price))
+
+    direction = get_direction(String.to_float(old_tick.price), String.to_float(event.price), old_tick.direction)
+
     ticks = Keyword.update!(
       socket.assigns.ticks,
       :"#{event.symbol}",
-      &(%{&1 | :price => event.price})
+      &(%{&1 | :price => event.price, :direction => "down"})
     )
 
     {:noreply, assign(socket, ticks: ticks)}
@@ -87,5 +107,13 @@ defmodule UiWeb.PriceFeedLive do
     |> Enum.map(&(Hefty.fetch_tick(&1)))
     |> Enum.into([], &{:"#{&1.symbol}", &1})
   end
+
+  defp get_direction(old, new, _) when old > new, do: "down"
+  defp get_direction(old, new, _) when old < new, do: "up"
+  defp get_direction(old, new, direction) when old == new, do: direction
+  defp get_direction() do
+    "up"
+  end
 end
+
 
