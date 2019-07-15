e9171443aeb56193ebdb15a089df644d1dde4e54
Piotr Wasilewski <piotr.wasilewski.uk@googlemail.com>
2019-07-15 22:28:15 +0100

    transactions page (#23)
    transactions page

---
+++ apps/hefty/lib/hefty/repo/transaction.ex
@@ -0,0 +1,26 @@
+defmodule Hefty.Repo.Transaction do
+  use Ecto.Schema
+
+  import Ecto.Query, only: [from: 2]
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "transactions" do
+    belongs_to(:order, Hefty.Repo.Binance.Order, foreign_key: :order_id, type: :binary_id)
+    field(:price, :string)
+    field(:quantity, :string)
+    field(:commission, :string)
+    field(:commission_asset, :string)
+
+    timestamps()
+  end
+
+  def fetch_transaction(id) do
+    query =
+      from(t in __MODULE__,
+        where: t.id == ^id
+      )
+
+    Hefty.Repo.one(query)
+  end
+end
