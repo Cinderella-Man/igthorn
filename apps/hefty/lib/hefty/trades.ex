ecc99d04a50b9c4a52358efbb9e439cdee498e2d
Kamil Skowron <emanuel1989@gmail.com>
2019-08-22 21:31:12 +0100

    Trade webpage fix, docs on tuning started

--- apps/hefty/lib/hefty/trades.ex
+++ apps/hefty/lib/hefty/trades.ex
@@ -4,56 +4,36 @@ defmodule Hefty.Trades do
   import Ecto.Query, only: [from: 2]
 
   alias Decimal, as: D
+  alias Hefty.Repo.Trade
   alias Hefty.Repo.Binance.Order
 
   @fee D.new(Application.get_env(:hefty, :trading).defaults.fee)
 
-  defmodule Trade do
-    defstruct id: nil,
-              symbol: nil,
-              buy_price: nil,
-              sell_price: nil,
-              quantity: nil,
-              state: nil,
-              profit_base_currency: nil,
-              profit_percentage: nil
-  end
-
   def fetch(offset, limit, symbol \\ "") do
-    Logger.debug("Fetching trades(based on orders) for a symbol(#{symbol})")
+    Logger.debug("Fetching trades for a symbol(#{symbol})")
 
-    # todo: Fetch current prices here
-
-    result =
-      from(o in Order,
-        select: [o.trade_id, o.symbol],
-        where: o.side == "BUY",
-        order_by: [desc: o.time],
+    trades =
+      from(t in Trade,
+        where: like(t.symbol, ^"%#{String.upcase(symbol)}%"),
+        order_by: [desc: t.sell_time],
         limit: ^limit,
         offset: ^offset
       )
       |> Hefty.Repo.all()
 
-    trade_ids = result |> Enum.map(&Enum.at(&1, 0))
-    symbols = result |> Enum.map(&Enum.at(&1, 1)) |> Enum.group_by(& &1) |> Map.keys()
+    symbols = trades |> Enum.map(& &1.symbol) |> Enum.group_by(& &1) |> Map.keys()
 
     current_prices = Hefty.TradeEvents.fetch_prices(symbols)
 
-    from(o in Order,
-      where: o.trade_id in ^trade_ids,
-      order_by: [desc: o.time]
-    )
-    |> Hefty.Repo.all()
-    |> Enum.group_by(& &1.trade_id)
-    |> Map.values()
-    |> Enum.map(&sum_up_trade(&1, current_prices))
+    trades
+    |> update_profit(current_prices)
   end
 
   def count(symbol \\ "") do
-    Logger.debug("Fetching count of trades(based on orders) for a symbol(#{symbol})")
+    Logger.debug("Fetching count of trades for a symbol(#{symbol})")
 
-    from(o in Order,
-      where: o.side == "BUY",
+    from(t in Trade,
+      where: t.symbol == ^"%#{String.upcase(symbol)}%",
       select: count("*"),
       limit: 1
     )
@@ -123,18 +103,26 @@ defmodule Hefty.Trades do
         %Order{:price => buy_price, :original_quantity => quantity},
         %Order{:price => sell_price}
       ) do
-    gross_invested = calculate_total_invested(buy_price, quantity, @fee)
-    net_sale = calculate_net_sale_amount(sell_price, quantity, @fee)
+    calculate_profit(buy_price, sell_price, quantity, @fee)
+  end
+
+  defp calculate_profit(buy_price, sell_price, quantity, fee) do
+    gross_invested = calculate_total_invested(buy_price, quantity, fee)
+    net_sale = calculate_net_sale_amount(sell_price, quantity, fee)
 
     D.sub(net_sale, gross_invested)
   end
 
   def calculate_profit_percentage(
-        %Order{:price => buy_price, :original_quantity => quantity} = buy_order,
-        %Order{} = sell_order
+        %Order{:price => buy_price, :original_quantity => quantity},
+        %Order{:price => sell_price}
       ) do
-    gross_invested = calculate_total_invested(buy_price, quantity, @fee)
-    profit = calculate_profit(buy_order, sell_order)
+    calculate_profit_percentage(buy_price, sell_price, quantity, @fee)
+  end
+
+  def calculate_profit_percentage(buy_price, sell_price, quantity, fee) do
+    gross_invested = calculate_total_invested(buy_price, quantity, fee)
+    profit = calculate_profit(buy_price, sell_price, quantity, fee)
     D.mult(D.div(profit, gross_invested), 100)
   end
 
@@ -154,50 +142,28 @@ defmodule Hefty.Trades do
     D.sub(sale_without_fee, D.mult(sale_without_fee, fee))
   end
 
-  # single buy order - trade depends on it's state
-  defp sum_up_trade([buy_order], _current_prices) do
-    state =
-      if buy_order.status == "CANCELLED" do
-        "CANCELLED"
-      else
-        "BUY PLACED"
-      end
+  defp update_profit(
+         %Trade{
+           buy_price: buy_price,
+           sell_price: nil,
+           quantity: quantity,
+           fee_rate: fee
+         } = trade,
+         current_prices
+       ) do
+    current_price = Map.get(current_prices, :price, 0)
 
-    %Trade{
-      :id => buy_order.trade_id,
-      :symbol => buy_order.symbol,
-      :buy_price => buy_order.price,
-      :quantity => buy_order.original_quantity,
-      :state => state,
-      :profit_base_currency => 0.0,
-      :profit_percentage => 0.0
-    }
-  end
+    profit_base_currency = D.to_float(calculate_profit(buy_price, current_price, quantity, fee))
 
-  # Possibilities here:
-  # 1 BUY + 1 SELL
-  # 1 BUY + 2 SELL (Stop loss)
-  defp sum_up_trade([sell_order | _] = orders, current_prices) do
-    buy_order = List.last(orders)
-
-    initial_trade = sum_up_trade([buy_order], current_prices)
-
-    state =
-      if sell_order.status == "FILLED" do
-        "COMPLETED"
-      else
-        "SELL PLACED"
-      end
-
-    profit_base_currency = D.to_float(calculate_profit(buy_order, sell_order))
-    profit_percentage = D.to_float(calculate_profit_percentage(buy_order, sell_order))
+    profit_percentage =
+      D.to_float(calculate_profit_percentage(buy_price, current_price, quantity, fee))
 
     %{
-      initial_trade
-      | :sell_price => sell_order.price,
-        :profit_base_currency => profit_base_currency,
-        :profit_percentage => profit_percentage,
-        :state => state
+      trade
+      | :profit_base_currency => profit_base_currency,
+        :profit_percentage => profit_percentage
     }
   end
+
+  defp update_profit(trade, _), do: trade
 end
---
+++ docs/tuning/test02.md
@@ -0,0 +1,27 @@
+Test 2
+
+Setting scaled down
+
+Settings:
+```
+    budget             | 1000
+    retarget_interval  | 0.01
+    profit_interval    | 0.0025
+    buy_down_interval  | 0.005
+    chunks             | 10
+    stop_loss_interval | 0.02
+    rebuy_interval     | 0.015
+```
+
+Symbol: XRPUSDT
+Date range: 2019-06-03 - 2019-06-09 (inclusive)
+Number of events: 1047065
+Market start price: 0.44396000
+Market last price: 0.38527000
+Market performance: -13.219659428777376%
+Naive strategy performance: -23.090458602525%
+Number of orders: 5138
+Number of trades: 1637
+    - gaining trades: 1357
+    - stop losses: 280
+Estimated number of retargeted orders = 1637 - 398 * 2 - 280 = 561
